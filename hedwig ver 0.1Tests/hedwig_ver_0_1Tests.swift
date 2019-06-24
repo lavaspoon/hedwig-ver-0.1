6c8d9a49c45ad99c44e68bfbe99d46760c80a4ae
lavaspoon <lavaspoon@naver.com>
2019-06-25 02:32:05 +0900

    Initial Commit

---
+++ hedwig ver 0.1Tests/hedwig_ver_0_1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  hedwig_ver_0_1Tests.swift
+//  hedwig ver 0.1Tests
+//
+//  Created by lavaspoon on 25/06/2019.
+//  Copyright © 2019 lavaspoon. All rights reserved.
+//
+
+import XCTest
+@testable import hedwig_ver_0_1
+
+class hedwig_ver_0_1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
