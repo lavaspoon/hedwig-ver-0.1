c61c45ebffac73ecb6d7e11cf177ad3dcfc261a6
lavaspoon <lavaspoon@naver.com>
2019-06-25 15:15:55 +0900

    190624.15:15

---
+++ hedwig ver 0.1/secondView.swift
@@ -0,0 +1,28 @@
+//
+//  secondView.swift
+//  hedwig ver 0.1
+//  Created by lavaspoon on 25/06/2019.
+//  Copyright © 2019 lavaspoon. All rights reserved.
+//
+import UIKit
+
+class secondView : UIViewController{
+   
+    
+    
+    @IBAction func backBtn(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+  
+}
