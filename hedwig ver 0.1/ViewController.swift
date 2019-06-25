8763436020f4366e0734575d1f21d459b023d805
lavaspoon <lavaspoon@naver.com>
2019-06-26 02:03:51 +0900

    19.06.24.02:03

--- hedwig ver 0.1/ViewController.swift
+++ hedwig ver 0.1/ViewController.swift
@@ -10,18 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let id = "root"
-    let password = "1234"
-
     @IBOutlet weak var uiIdInput: UITextField!
     @IBOutlet weak var uiPasswordInput: UITextField!
     
+    var id = String()
+    var password = String()
     
+    // DB 저장되는 파일 경로
+    var databasePath = String()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // < DB 설정 : DB를 사용하기 위해 미리 파일을 구성하고 테입르을 삽입하는 과정 > //
+        
+        let fileMgr = FileManager.default // FileManager의 인스턴스 생성
+        let dirPath = NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true) // 복잡한 경로 기억하는 함수, 앱의 Document 경로 찾아서 반환해줌
+        let docsDir = dirPath[0]
+        databasePath = docsDir.appending("/contacts.db")
+        
+        if !fileMgr.fileExists(atPath: databasePath) { // 파일 존재 여부 판별, 없으면 생성하고 테이블을 구성하는 쿼리 실행
+            let contactDB = FMDatabase(path: databasePath) // FMDatabase 클래스의 인스턴스 생성
+            
+            if contactDB.open() {
+                let sql1 = "CREATE TABLE IF NOT EXISTS MEMBER (ID TEXT PRIMARY KEY, PASSWORD TEXT, NAME TEXT, AGE INTEGER)"
+                // if not exists test : 테이블이 존재하지 않으면
+                // 실행할 쿼리를 sql1 상수에 미리 저장하고 executeStatements로 구문을 실행해 실제 DB 테이블 생성
+                let sql2 = "INSERT INTO MEMBER VALUES ('root', '1234', '운영자', '26'"
+                if !contactDB.executeStatements(sql1) {
+                    NSLog("SQL 오류")
+                }
+                if !contactDB.executeStatements(sql2) {
+                    NSLog("SQL 오류")
+                }
+                contactDB.close()
+            } else {
+                NSLog("contactDB 열기 실패")
+            }
+        } else {
+            NSLog("contactDB가 존재")
+        }
+        // < DB 설정 종료 > //
+    }
+
     @IBAction func loginClicked(_ sender: Any) {
         
         let userId = uiIdInput.text
         let userPassword = uiPasswordInput.text
-    
+        
+        // ID 조회
+        let contactDB = FMDatabase(path: databasePath)
+        contactDB.open()
+        let selectsql = "SELECT * FROM MEMBER WHERE ID = '\(userId!)'"
+        do {
+            let result = try contactDB.executeQuery(selectsql, values: [])
+            if result.next() {
+                id = result.string(forColumn: "ID")!
+                password = result.string(forColumn: "PASSWORD")!
+            }
+        } catch {
+            NSLog("ID 조회 DB 오류")
+        }
+        // ID 조회
+        
         let alert = UIAlertController(
             title: "알림창",
             message: "아이디: \(userId!), 비밀번호: \(userPassword!)",
@@ -42,14 +93,6 @@ class ViewController: UIViewController {
         present(alert,animated: true, completion: nil)
         
     }
-    
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-       
-    }
-
 
 }
 
